6317352cf022cc097e5aa651df8f3069961cc0e2
Jeferson.Rocha <tecjeferson@gmail.com>
2019-06-06 17:36:07 -0300

    fixing Public

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({
     extended: true
 }))
