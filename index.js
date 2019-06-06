7bf8876e3f332f5af0eacda40bd8d43d835c1b33
Jeferson.Rocha <tecjeferson@gmail.com>
2019-06-06 16:24:19 -0300

    fixing identity

--- index.js
+++ index.js
@@ -40,19 +40,27 @@ app.get('/vaga/:id', async (request, response) => {
     response.render('vaga', {
         vaga
     })
-}) app.get('/admin', (req, res) => {
+})
+
+app.get('/admin', (req, res) => {
     res.render('admin/home')
-}) app.get('/admin/vagas', async (req, res) => {
+})
+
+app.get('/admin/vagas', async (req, res) => {
     const db = await dbConnection
     const vagas = await db.all('select * from vagas;')
     res.render('admin/vagas', {
         vagas
     })
-}) app.get('/admin/vagas/delete/:id', async (req, res) => {
+})
+
+app.get('/admin/vagas/delete/:id', async (req, res) => {
     const db = await dbConnection
     await db.run('delete from vagas where id = ' + req.params.id + '')
     res.redirect('/admin/vagas')
-}) app.get('/admin/vagas/nova', async (req, res) => {
+})
+
+app.get('/admin/vagas/nova', async (req, res) => {
     const db = await dbConnection
     const categorias = await db.all('select * from categorias')
     res.render('admin/nova-vaga', {
@@ -68,7 +76,9 @@ app.get('/admin/vagas/editar/:id', async (req, res) => {
         categorias,
         vaga
     })
-}) app.post('/admin/vagas/nova', async (req, res) => {
+})
+
+app.post('/admin/vagas/nova', async (req, res) => {
     const {
         titulo,
         descricao,
@@ -77,7 +87,9 @@ app.get('/admin/vagas/editar/:id', async (req, res) => {
     const db = await dbConnection
     await db.run(`insert into vagas(categoria, titulo, descricao) values(${categoria},'${titulo}','${descricao}')`)
     res.redirect('/admin/vagas')
-}) app.post('/admin/vagas/editar/:id', async (req, res) => {
+})
+
+app.post('/admin/vagas/editar/:id', async (req, res) => {
     const {
         titulo,
         descricao,
