69ec43edcda173fb33033953eed6dc1a590b0da4
Jeferson.Rocha <tecjeferson@gmail.com>
2019-06-06 17:42:36 -0300

    adding IF to only localhost can access the Admin area

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), {
 
 const port = process.env.PORT || 3000
 
+app.use('/admin', (req, res, next) => {
+    if (req.hostname === 'localhost') {
+        next()
+    } else {
+        res.send('Not allowed')
+    }
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
